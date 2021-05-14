5d5eac0cdfa02600d0be861ceca3e8a078256223
pyoseol <80082549+pyoseol@users.noreply.github.com>
2021-05-14 11:56:37 +0900

    30203

---
+++ Assets/Scripts/PLAYERCONTROLLER.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLAYERCONTROLLER : MonoBehaviour
+{
+    private float translation;
+    private float rotation;
+
+    public float tspeed = 10f;
+    public float rspeed = 100f;
+    void Update() {
+        translation = Input.GetAxis("Vertical") * 10f; 
+        rotation = Input.GetAxis("Horizontal") * 100f;
+
+        transform.Translate(0, 0, translation* Time.deltaTime);
+        transform.Rotate(0, rotation* Time.deltaTime, 0);
+    }
+}
