a312553a9464f35a6f8169595e0f903beb648365
pyoseol <80082549+pyoseol@users.noreply.github.com>
2021-05-21 12:01:27 +0900

    30203

--- Assets/Scripts/PLAYERCONTROLLER.cs
+++ Assets/Scripts/PLAYERCONTROLLER.cs
@@ -4,16 +4,41 @@ using UnityEngine;
 
 public class PLAYERCONTROLLER : MonoBehaviour
 {
-    private float translation;
+    private float translation; 
     private float rotation;
 
-    public float tspeed = 10f;
-    public float rspeed = 100f;
-    void Update() {
-        translation = Input.GetAxis("Vertical") * 10f; 
+    private Animator ani;
+
+    private void Start()
+    {
+        ani = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        translation = Input.GetAxis("Vertical") * 10f;
         rotation = Input.GetAxis("Horizontal") * 100f;
 
-        transform.Translate(0, 0, translation* Time.deltaTime);
-        transform.Rotate(0, rotation* Time.deltaTime, 0);
+        transform.Translate(0, 0, translation * Time.deltaTime);
+        transform.Rotate(0, rotation * Time.deltaTime, 0);
+
+        if (translation != 0 || rotation != 0)
+        {
+            ani.SetBool("walk", true);
+        }
+        else
+        {
+            ani.SetBool("walk", false);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Obstacle")
+        {
+            ani.SetTrigger("damage");
+        }
     }
 }
+
+
